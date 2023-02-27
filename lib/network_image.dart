efd7367b2fad4438c2f39e84736b1f563632cddc
Ko Ko Lin Oo <linoo.koko.dev@gmail.com>
2023-02-27 11:21:16 +0630

    network image

--- lib/network_image.dart
+++ lib/network_image.dart
@@ -1,19 +1,31 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class PNetworkImage extends StatelessWidget {
-  const PNetworkImage(this.imageUrl, {super.key});
+  const PNetworkImage(this.imageUrl, {super.key, this.radius = 10});
 
   final String imageUrl;
+  final double radius;
 
   @override
   Widget build(BuildContext context) {
     return CachedNetworkImage(
       imageUrl: imageUrl,
-      progressIndicatorBuilder: (context, url, progress) {
-        return const CircularProgressIndicator();
-      },
+      progressIndicatorBuilder: (context, url, downloadProgress) =>
+          CircularProgressIndicator(value: downloadProgress.progress),
+      imageBuilder: (context, imageProvider) => Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(radius),
+          image: DecorationImage(
+            image: imageProvider,
+            fit: BoxFit.cover,
+            colorFilter: const ColorFilter.mode(
+              Colors.red,
+              BlendMode.colorBurn,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
